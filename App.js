e7614aa353a3599d02411e6f459378be91945e09
DiwenLiu <liud7879@gmail.com>
2024-03-25 15:15:46 -0400

    init client

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import HomeScreen from './screens/HomeScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import FriendScreen from './screens/FriendScreen';
 
-export default function App() {
+const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          switch (route.name) {
+            case 'Friends':
+              iconName = focused ? 'account-group' : 'account-group-outline';
+              break;
+            case 'Message':
+              iconName = focused ? 'message-text' : 'message-text-outline';
+              break;
+            default:
+              break;
+          }
+          return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+        },
+        "tabBarActiveTintColor": "blue",
+        "tabBarInactiveTintColor": "gray",
+      })}>
+      <Tab.Screen options={{ headerShown: false }} name="Message" component={HomeScreen} />
+      <Tab.Screen options={{ headerShown: false }} name="Friends" component={FriendScreen} />
+    </Tab.Navigator>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+  return (
+    // <Authenticator.Provider>
+    //   <Authenticator signUpAttributes={[
+    //     "email",
+    //     "name"
+    //   ]}>
+        <NavigationContainer>
+          <Drawer.Navigator initialRouteName="Tabs">
+            <Drawer.Screen name="Home" component={TabNavigator}/>
+            <Drawer.Screen name="Profile" component={ProfileScreen} />
+          </Drawer.Navigator>
+        </NavigationContainer>
+    //   </Authenticator>
+    // </Authenticator.Provider>
+  );
+};
+
+export default App;
---
+++ screens/FriendScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const FriendScreen: React.FC = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Friend Screen</Text>
+    </View>
+  );
+};
+
+export default FriendScreen;
